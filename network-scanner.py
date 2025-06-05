49dbe27a9cd6351c927efcf76a7b48bb4e0196e6
Marcelo Oviedo <oviedommarcelo@gmail.com>
2025-06-04 22:33:49 -0300

    Se cambian parámetros

--- network-scanner.py
+++ network-scanner.py
@@ -2,14 +2,16 @@ import nmap
 
 def scan_network(ip_range):
     scanner = nmap.PortScanner()
-    scanner.scan(hosts=ip_range, arguments='-F -sV')  # Escaneo rápido + detección de servicios
+    scanner.scan(
+        hosts=ip_range, 
+        arguments='-T4 -p 1-65535 -sV --open')  # Todos los puertos + solo abiertos +
     
-    print(f"Resultados del escaneo en {ip_range}:")
-    for host in scanner.all_hosts():
+    print(f"\n🔍 Escaneo completado en {ip_range} (Puertos abiertos):")
+    for host in scanner.all_hosts(): #Este for recorre todos los host en caso de que el parámetro sean varias IPs o un CIDR
         print(f"\nHost: {host} ({scanner[host].hostname()})")
         print(f"Estado: {scanner[host].state()}")
         
-        for proto in scanner[host].all_protocols():
+        for proto in scanner[host].all_protocols(): #Para cada host mostramos los puertos abiertos y el detalle
             ports = scanner[host][proto].keys()
             for port in ports:
                 service = scanner[host][proto][port]['name']
@@ -17,4 +19,4 @@ def scan_network(ip_range):
 
 # Ejemplo de uso
 print('Inicio de pruebas: -----------------------------------------')
-scan_network("192.168.1.0/24")  # Escanear toda una subred
+scan_network("192.168.1.8")  # Enviamos la/s IP a escanear o un CIDR del formato
